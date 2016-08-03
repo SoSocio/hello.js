9e05769701a111473475193bf72e1e2722f96b30
Bjorn Bos <bjorn.bos@sosocio.com>
2016-08-03 16:25:03 +0200

    CHANGED dropbox route mapping

--- src/modules/dropbox.js
+++ src/modules/dropbox.js
@@ -72,7 +72,7 @@
 				// Https://www.dropbox.com/developers/core/docs#metadata
 				'me/files': req('metadata/@{parent|}'),
 				'me/folder': req('metadata/dropbox/@{id}'),
-				'me/folders': req('metadata/'),
+				'me/folders': req('metadata/dropbox'),
 
 				'default': function(p, callback) {
 					if (p.path.match('https://api-content.dropbox.com/1/files/')) {
